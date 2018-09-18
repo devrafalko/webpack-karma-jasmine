fa24283b30d750c85bcece373e14732db0678df6
Fulvio Casali <fulviocasali@gmail.com>
2018-09-18 16:23:59 -0700

    Fix build issue in sample.

--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         exclude:/(node_modules)/,
         loader:'babel-loader',
         options:{
-          presets:['env']
+          presets:['@babel/env']
         }
       }
     ]
